b898fbadf0b6dd9ff5dfadbc9fd52689728f42c1
LordNX <74880769+LordNX@users.noreply.github.com>
2021-12-04 02:02:27 -0600

    XD

---
+++ app/src/main/java/mx/jjpg/proyecto/app/personaje.kt
@@ -0,0 +1,17 @@
+package mx.jjpg.proyecto.app
+
+class personaje{
+    var colorP : String
+    var generoP : String
+    var armasP: String
+    var animalP: String
+
+
+
+    constructor(colorP: String, generoP: String,armasP:String,animalP:String) {
+        this.colorP = colorP
+        this.generoP = generoP
+        this.armasP = armasP
+        this.animalP = animalP
+    }
+}
